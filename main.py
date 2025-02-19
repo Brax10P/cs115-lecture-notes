a02b3ae13cde0afb8f11c9f0d4f52d82f8ef08c4
Brax10P <bprendergast12@hotmail.com>
2025-02-19 19:15:39 +0000

    we've added a lot of examples on how to code in Python

---
+++ main.py
@@ -0,0 +1,38 @@
+print("Hello World")
+
+my_variable = 6
+num_cats = 8
+
+print(num_cats)
+print("num_cats")
+
+print("num cats: " , num_cats)
+print(f"number of cats {num_cats}")
+print(f'my age {my_variable} and my cats {num_cats}')
+
+answer = input("What is your name?")
+print(f'You answered: {answer}')
+
+x = 6
+y = 20
+print(f'x = {x}')
+c = x+y 
+print(c)
+
+# arithmetic operations
+print(f'c = {x+y}')
+print(f'x*y = {x*y}')
+
+# this is a comment
+# I can take a lot of notes here
+
+""" 
+This is a multiline comment
+anything I type here will be ignored
+
+"""
+
+num1 = input("What is the first number?")
+num2 = input("What is the second number?")
+sum = int(num1) + int(num2)
+print(f'{num1} + {num2} = {sum}')
