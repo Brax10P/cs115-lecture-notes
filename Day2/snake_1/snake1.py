ecf552a29aca85fca169bd41e5bdc352d95b65a7
Brax10P <bprendergast12@hotmail.com>
2025-03-03 19:08:34 +0000

    Took day 4 notes and added a bunch of code to snake

--- Day2/snake_1/snake1.py
+++ Day2/snake_1/snake1.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.constants import KEYDOWN
 
 #init pygame
 pygame.init() 
@@ -16,6 +17,17 @@ clock = pygame.time.Clock()
 dt = 0
 speed = 10
 
+#Setting Snake Current Position
+cur_pos = [300,200]
+direction = "down"
+snake_body = [
+  [300,200],
+  [300,180],
+  [300,160],
+  [300,140]
+]
+
+
 """ Game Loop """
 running = True
 while running:
@@ -23,35 +35,64 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.quit:
       running = False
+    if event.type == KEYDOWN:
+      if event.key == pygame.K_ESCAPE: #escape key
+        running = False
+      if event.key == pygame.K_w and direction != "down": #up direction
+        direction = "up" #y-coord
+      if event.key == pygame.K_s and direction != "up": #down
+        direction = "down" #y-coord
+      if event.key == pygame.K_a and direction != "right": #left
+        direction = "left" #x-coord
+      if event.key == pygame.K_d and direction != "left": #right
+        direction = "right" #x-coord
+        
+        
 
 
   """Update Our Game State"""
+  #update direction
+  if direction == "up":
+    cur_pos[1] -= 20
+  if direction == "down":
+    cur_pos[1] += 20
+  if direction == "left":
+    cur_pos[0] -= 20
+  if direction == "right":
+    cur_pos[0] += 20
 
+  if cur_pos[0] < 0:
+    cur_pos[0] = 0
+    running = False
+  if cur_pos[0] > width-20:
+    cur_pos[0] = width-20 
+    running = False
+  if cur_pos[1] < 0:
+    cur_pos[1] = 0
+    running = False
+  if cur_pos[1] > height-20: 
+    cur_pos[1] = height-20
+    running = False
+
+
+  #Snake Movement
+  snake_body.insert(0, list(cur_pos))
+  snake_body.pop()
+
+  #handle lose state
 
   """Draw to our Screen"""
   #clear screen
   screen.fill("white")
 
-  #draw rectangle
-  pygame.draw.rect(
-    screen, 
-    "blue", 
-    pygame.Rect((100,200), (100,50)))
+  #draw snake
+  for body in snake_body:
+    pygame.draw.rect(
+      screen, 
+      "blue", 
+      pygame.Rect((body[0], body[1]), (20,20)))
   
-  #Draw Circle
-  pygame.draw.circle(
-    screen, 
-    "green", 
-    (100,200), 
-    40)
 
-  #Draw a Line
-  pygame.draw.line(
-    screen, 
-    "red", 
-    (100,100), 
-    (200,200), 
-    5)
 
 
   #update screen
---
+++ Day4/day4.py
@@ -0,0 +1,14 @@
+"""Day 4 Notes"""
+""""
+a = 10
+b = 5
+
+if a < b:
+  print("A is less than B")
+  result = b - a 
+else:
+  print("B is less than A")
+  result = a - b
+
+  """
+
